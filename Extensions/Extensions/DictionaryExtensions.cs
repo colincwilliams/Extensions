7f4c09c1f0e0ef20612a2fa287a27b646e5e96b0
Colin Williams <colin@colincwilliams.com>
2016-04-08 17:58:07 -0700

    First check-in.

---
+++ Extensions/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,78 @@
+﻿namespace ColinCWilliams.Extensions
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public static class DictionaryExtensions
+    {
+        private const uint PrettyPrintMinSeparation = 4;
+        private const char PrettyPrintDefaultSeparator = ' ';
+
+        public static string PrettyPrint<TKey, TValue>(
+            this Dictionary<TKey, TValue> dictionary,
+            char separator = PrettyPrintDefaultSeparator,
+            uint minSeparation = PrettyPrintMinSeparation)
+        {
+            dictionary.ThrowIfNull(nameof(dictionary));
+            return dictionary.AsEnumerable().PrettyPrint(separator, minSeparation);
+        }
+
+        public static string PrettyPrint<TKey, TValue>(
+            this IDictionary<TKey, TValue> dictionary,
+            char separator = PrettyPrintDefaultSeparator,
+            uint minSeparation = PrettyPrintMinSeparation)
+        {
+            dictionary.ThrowIfNull(nameof(dictionary));
+            return dictionary.AsEnumerable().PrettyPrint(separator, minSeparation);
+        }
+
+        public static string PrettyPrint<TKey, TValue>(
+            this IReadOnlyDictionary<TKey, TValue> dictionary,
+            char separator = PrettyPrintDefaultSeparator,
+            uint minSeparation = PrettyPrintMinSeparation)
+        {
+            dictionary.ThrowIfNull(nameof(dictionary));
+            return dictionary.AsEnumerable().PrettyPrint(separator, minSeparation);
+        }
+
+        public static string PrettyPrint<TKey, TValue>(
+            this IEnumerable<KeyValuePair<TKey, TValue>> kvPairs,
+            char separator = PrettyPrintDefaultSeparator,
+            uint minSeparation = PrettyPrintMinSeparation)
+        {
+            kvPairs.ThrowIfNull(nameof(kvPairs));
+
+            string printed = string.Empty;
+
+            if (kvPairs.Any())
+            {
+                StringBuilder sb = new StringBuilder();
+
+                uint maxKeyLength = kvPairs.Max(kvPair => GetStringLength(kvPair.Key));
+
+                uint totalKeyLength = maxKeyLength + minSeparation;
+
+                foreach (var pair in kvPairs)
+                {
+                    sb.Append(pair.Key);
+
+                    uint keyLength = GetStringLength(pair.Key);
+                    sb.RepeatAppend(separator, totalKeyLength - keyLength);
+
+                    sb.Append(pair.Value);
+                    sb.AppendLine();
+                }
+
+                printed = sb.ToString();
+            }
+
+            return printed;
+        }
+
+        private static uint GetStringLength(object o)
+        {
+            return o == null ? 0 : (uint)o.ToString().Length;
+        }
+    }
+}
---
+++ Extensions/Extensions/Extensions.cs
@@ -0,0 +1,60 @@
+﻿namespace ColinCWilliams.Extensions
+{
+    using System;
+    using System.Text;
+
+    public static class Extensions
+    {
+        /// <summary>
+        /// Parses a string to an enum value from enum type T, or returns the provided default value
+        /// if the parse fails. A parse is considered to have failed if the resulting value does
+        /// not match a defined value in the enum type T.
+        /// </summary>
+        /// <typeparam name="T">The enum type to parse from.</typeparam>
+        /// <param name="str">The string to parse.</param>
+        /// <param name="defaultValue">The default value to return if parsing fails.</param>
+        /// <returns></returns>
+        public static T ParseEnum<T>(this string str, T defaultValue) where T : struct
+        {
+            T result;
+            if (str == null || !Enum.TryParse(str, out result) || !Enum.IsDefined(typeof(T), result))
+            {
+                result = defaultValue;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Appends an object to a StringBuilder the specified number of times.
+        /// </summary>
+        /// <param name="stringBuilder">The StringBuilder to append to.</param>
+        /// <param name="obj">The object to append.</param>
+        /// <param name="count">The number of times to append the object.</param>
+        /// <returns></returns>
+        public static StringBuilder RepeatAppend(this StringBuilder stringBuilder, object obj, uint count)
+        {
+            stringBuilder.ThrowIfNull(nameof(stringBuilder));
+
+            for (int i = 0; i < count; i++)
+            {
+                stringBuilder.Append(obj);
+            }
+
+            return stringBuilder;
+        }
+
+        /// <summary>
+        /// Throws an ArgumentNullException with the provided argument name if the object is null.
+        /// </summary>
+        /// <param name="obj">The object to check for null.</param>
+        /// <param name="argName">The argument name to use in the ArgumentNullException.</param>
+        public static void ThrowIfNull(this object obj, string argName)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(argName);
+            }
+        }
+    }
+}
